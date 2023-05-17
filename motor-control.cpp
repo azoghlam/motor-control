b7e033c3415417b9ba7b45b9abd2bc4a5b0a8bc0
azoghlam <123667148+azoghlam@users.noreply.github.com>
2023-05-17 16:06:16 -0400

    Create motor-control.cpp

---
+++ motor-control.cpp
@@ -0,0 +1,36 @@
+#include <wiringPi.h>
+#include <iostream>
+
+#define pwm 4 // pin7
+#define IN1 27// pin13
+#define IN2 17//pin11
+
+using namespace std;
+
+int main(){
+ wiringPiSetup () ;
+
+pinMode(pwm, OUTPUT) ;
+pinMode(IN1, OUTPUT) ;
+pinMode(IN2, OUTPUT) ;
+ while (1)
+ {
+
+digitalWrite(pwm, HIGH);
+digitalWrite(IN1, HIGH);
+digitalWrite(IN2, LOW) ;
+delay(1000);
+
+
+digitalWrite(pwm, HIGH);
+digitalWrite(IN1, LOW);
+digitalWrite(IN2, HIGH) ;
+delay(1000);
+
+
+ }
+
+
+
+return 0 ;
+}
