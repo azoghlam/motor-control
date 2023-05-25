5c07fae3a3049839a373a2de6b10a159e60b4beb
azoghlam <123667148+azoghlam@users.noreply.github.com>
2023-05-25 12:05:24 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -90,13 +90,9 @@ short read_raw_data(int address)
 	short high_byte,low_byte,value;
 	high_byte = wiringPiI2CReadReg8(AK8963_DEVICE_ADDR , address);
 	low_byte = wiringPiI2CReadReg8(AK8963_DEVICE_ADDR , address-1);
-	value = (high_byte << 8) | low_byte;
+	value = (low_byte << 8) |high_byte ;
 
-    if (value > 32768)
-    {
-        value = 32768 -65536;
-
-    } 
+ 
 
 	return value;
 }
