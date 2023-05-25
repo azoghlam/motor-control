28b16334cb3cc0c131db20e75aa70aba6aa1b37f
azoghlam <123667148+azoghlam@users.noreply.github.com>
2023-05-25 19:35:37 -0400

    Update mpu9250_i2c.py

--- mpu9250_i2c.py
+++ mpu9250_i2c.py
@@ -3,6 +3,7 @@
 # refer to datasheet and register map for full explanation
 
 import smbus2,time
+import math
 
 def MPU6050_start():
     # alter sample rate (stability)
@@ -54,25 +55,36 @@ def mpu6050_conv():
     acc_y = read_raw_bits(ACCEL_YOUT_H)
     acc_z = read_raw_bits(ACCEL_ZOUT_H)
 
+    
+    angleAccX = math.atan2 (acc_y, math.sqrt( acc_z *  acc_z  +acc_x * acc_x)) * 180 / 3.141592
+    angleAccY = math.atan2 (acc_x, math.sqrt( acc_z  *  acc_z  + acc_y * acc_y)) * 180 / 3.141592
+    
+    angleX = 0.98*angleX + 0.02*angleAccX
+    angleY = 0.98*angleY + 0.02*angleAccY
+
     # raw temp bits
 ##    t_val = read_raw_bits(TEMP_OUT_H) # uncomment to read temp
     
     # raw gyroscope bits
-    gyro_x = read_raw_bits(GYRO_XOUT_H)
-    gyro_y = read_raw_bits(GYRO_YOUT_H)
-    gyro_z = read_raw_bits(GYRO_ZOUT_H)
+  #  gyro_x = read_raw_bits(GYRO_XOUT_H)
+   # gyro_y = read_raw_bits(GYRO_YOUT_H)
+   # gyro_z = read_raw_bits(GYRO_ZOUT_H)
 
     #convert to acceleration in g and gyro dps
-    a_x = (acc_x/(2.0**15.0))*accel_sens
-    a_y = (acc_y/(2.0**15.0))*accel_sens
-    a_z = (acc_z/(2.0**15.0))*accel_sens
+   # a_x = (acc_x/(2.0**15.0))*accel_sens
+   # a_y = (acc_y/(2.0**15.0))*accel_sens
+   # a_z = (acc_z/(2.0**15.0))*accel_sens
 
-    w_x = (gyro_x/(2.0**15.0))*gyro_sens
-    w_y = (gyro_y/(2.0**15.0))*gyro_sens
-    w_z = (gyro_z/(2.0**15.0))*gyro_sens
+   # w_x = (gyro_x/(2.0**15.0))*gyro_sens
+   # w_y = (gyro_y/(2.0**15.0))*gyro_sens
+   # w_z = (gyro_z/(2.0**15.0))*gyro_sens
 
 ##    temp = ((t_val)/333.87)+21.0 # uncomment and add below in return
-    return a_x,a_y,a_z,w_x,w_y,w_z
+   # return a_x,a_y,a_z,w_x,w_y,w_z
+    
+    return angleX,angleY
+
+
 
 def AK8963_start():
     bus.write_byte_data(AK8963_ADDR,AK8963_CNTL,0x00)
