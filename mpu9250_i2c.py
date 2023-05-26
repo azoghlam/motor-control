fbeca584b0640d4831f6a7b060dec73f44ee5aac
azoghlam <123667148+azoghlam@users.noreply.github.com>
2023-05-26 12:00:15 -0400

    Update mpu9250_i2c.py

--- mpu9250_i2c.py
+++ mpu9250_i2c.py
@@ -67,9 +67,9 @@ def mpu6050_conv():
    # angleAccY = math.atan2 (ax, math.sqrt( az *  az + ay * ay)) * 180 / math.pi
    # angleAccZ = math.atan2 (math.sqrt( ax  *  ax + ay * ay), az) * 180 / math.pi
     
-    angleAccX = 0.98 * angleAccX + 0.02 * (math.atan2 ( ay , math.sqrt( az *  az  +ax * ax)) * 180 / math.pi)
-    angleAccY = 0.98 * angleAccY + 0.02 * (math.atan2 (ax, math.sqrt( az *  az + ay * ay)) * 180 / math.pi)
-    angleAccZ = 0.98 * angleAccZ + 0.02 * (math.atan2 (math.sqrt( ax  *  ax + ay * ay), az) * 180 / math.pi)
+    angleAccX =  0.02 * (math.atan2 ( ay , math.sqrt( az *  az  +ax * ax)) * 180 / math.pi)
+    angleAccY =  0.02 * (math.atan2 (ax, math.sqrt( az *  az + ay * ay)) * 180 / math.pi)
+    angleAccZ =  0.02 * (math.atan2 (math.sqrt( ax  *  ax + ay * ay), az) * 180 / math.pi)
 
 
    # angleX = 0.98*angleX + 0.02*angleAccX
