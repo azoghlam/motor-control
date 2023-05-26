0e0a290cbb2438b3298feb4778ee9b9b4253f53f
azoghlam <123667148+azoghlam@users.noreply.github.com>
2023-05-25 20:21:22 -0400

    Update mpu9250_i2c.py

--- mpu9250_i2c.py
+++ mpu9250_i2c.py
@@ -83,9 +83,9 @@ def mpu6050_conv():
 ##    temp = ((t_val)/333.87)+21.0 # uncomment and add below in return
     #return a_x,a_y,a_z,w_x,w_y,w_z
     
-    angleAccX = angleX 
-    angleAccY = angleY
-    print(angleX)
+    #angleAccX = angleX 
+    #angleAccY = angleY
+
 
     return  angleX,angleY
 
