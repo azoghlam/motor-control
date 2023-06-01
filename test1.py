ffd6481bc59a8ed9ce68ea3067a7b02eb23e565b
azoghlam <123667148+azoghlam@users.noreply.github.com>
2023-06-01 15:27:02 -0400

    Update test1.py

--- test1.py
+++ test1.py
@@ -14,7 +14,7 @@ while 1:
     print('accel [g]: x = {0:2.2f}, y = {1:2.2f}, z {2:2.2f}= '.format(ax,ay,az))
     print('gyro [dps]:  x = {0:2.2f}, y = {1:2.2f}, z = {2:2.2f}'.format(wx,wy,wz))
     print('mag [uT]:   x = {0:2.2f}, y = {1:2.2f}, z = {2:2.2f}'.format(mx,my,mz))
-    print('heading:   x = {0:2.2f}'.format(mt))
+   # print('heading:   x = {0:2.2f}'.format(mt))
     print('{}'.format('-'*30))
    
     time.sleep(1)
