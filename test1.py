80c7dc649eb558eb3bc10ea4a79994f815c4c3ff
azoghlam <123667148+azoghlam@users.noreply.github.com>
2023-06-01 14:12:21 -0400

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,13 +1,12 @@
-import magnetometer
-import time
+from  magnetometer import *
 
 time.sleep(1) # delay necessary to allow mpu9250 to settle
 
 print('recording data')
 while 1:
     try:
-        ax,ay,az,wx,wy,wz = magnetometer.mpu6050_conv() # read and convert mpu6050 data
-        mx,my,mz = magnetometer.AK8963_conv() # read and convert AK8963 magnetometer data
+        ax,ay,az,wx,wy,wz = mpu6050_conv() # read and convert mpu6050 data
+        mx,my,mz = AK8963_conv() # read and convert AK8963 magnetometer data
     except:
         continue
     
