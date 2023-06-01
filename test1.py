ffb4fffcd83767a53877f4865147edc613853f70
azoghlam <123667148+azoghlam@users.noreply.github.com>
2023-06-01 17:04:21 -0400

    Update test1.py

--- test1.py
+++ test1.py
@@ -3,8 +3,6 @@ from  mpu9250_i2c import *
 import socketio
 sio = socketio.Client()
 
-
-
 @sio.event
 def disconnect():
     print('disconnected from server')
@@ -21,14 +19,17 @@ def connect():
     initLoop()
 
 def initLoop ():
-    #  while 1:
-    #     try:
-        ax,ay,az,wx,wy,wz = mpu6050_conv() # read and convert mpu6050 data
-        mx,my,mz,heading = AK8963_conv() # read and convert AK8963 magnetometer data
-        # except:
-            # continue
+     while 1:
+        try:
+            ax,ay,az,wx,wy,wz = mpu6050_conv() # read and convert mpu6050 data
+            mx,my,mz,heading = AK8963_conv() # read and convert AK8963 magnetometer data
+        except:
+            continue
         
-        sio.emit('gyro',ax)
+        sio.emit('gyro',ax,ay,az)
+        sio.emit('acc',wx,wy,wz)
+        sio.emit('mag',mx,my,mz)
+        # sio.emit('heading',heading)
         # print('{}'.format('-'*30))
         # print('accel [g]: x = {0:2.2f}, y = {1:2.2f}, z {2:2.2f}= '.format(ax,ay,az))
         # print('gyro [dps]:  x = {0:2.2f}, y = {1:2.2f}, z = {2:2.2f}'.format(wx,wy,wz))
