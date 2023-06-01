70705ac644e8f2089f0d6a0cca1ddf130d92f57e
azoghlam <123667148+azoghlam@users.noreply.github.com>
2023-06-01 16:49:33 -0400

    Update test1.py

--- test1.py
+++ test1.py
@@ -3,12 +3,6 @@ from  mpu9250_i2c import *
 import socketio
 sio = socketio.Client()
 
-
-@sio.event
-def connect():
-    print('connection established')
-    sio.emit("ID", 'python-gyro-client')
-
 @sio.event
 def disconnect():
     print('disconnected from server')
@@ -20,23 +14,31 @@ sio.wait()
 
 time.sleep(1) # delay necessary to allow mpu9250 to settle
 
-print('recording data')
-while 1:
-    try:
-        ax,ay,az,wx,wy,wz = mpu6050_conv() # read and convert mpu6050 data
-        mx,my,mz,heading = AK8963_conv() # read and convert AK8963 magnetometer data
-    except:
-        continue
+
+@sio.event
+def connect():
+    print('connection established')
+    sio.emit("ID", 'python-gyro-client')
+    print('recording data')
+    while 1:
+        try:
+            ax,ay,az,wx,wy,wz = mpu6050_conv() # read and convert mpu6050 data
+            mx,my,mz,heading = AK8963_conv() # read and convert AK8963 magnetometer data
+        except:
+            continue
+        
+        sio.emit('gyro',ax,ay,az)
+        sio.emit('acc',wx,wy,wz)
+        sio.emit('mag',mx,my,mz)
+        sio.emit('heading',heading)
+        # print('{}'.format('-'*30))
+        # print('accel [g]: x = {0:2.2f}, y = {1:2.2f}, z {2:2.2f}= '.format(ax,ay,az))
+        # print('gyro [dps]:  x = {0:2.2f}, y = {1:2.2f}, z = {2:2.2f}'.format(wx,wy,wz))
+        # print('mag [uT]:   x = {0:2.2f}, y = {1:2.2f}, z = {2:2.2f}'.format(mx,my,mz))
+        # print('heading:   x = {0:2.2f}'.format(heading))
+        # print('{}'.format('-'*30))
     
-    sio.emit('gyro',ax,ay,az)
-    sio.emit('acc',wx,wy,wz)
-    sio.emit('mag',mx,my,mz)
-    sio.emit('heading',heading)
-    # print('{}'.format('-'*30))
-    # print('accel [g]: x = {0:2.2f}, y = {1:2.2f}, z {2:2.2f}= '.format(ax,ay,az))
-    # print('gyro [dps]:  x = {0:2.2f}, y = {1:2.2f}, z = {2:2.2f}'.format(wx,wy,wz))
-    # print('mag [uT]:   x = {0:2.2f}, y = {1:2.2f}, z = {2:2.2f}'.format(mx,my,mz))
-    # print('heading:   x = {0:2.2f}'.format(heading))
-    # print('{}'.format('-'*30))
-   
-    # time.sleep(1)
+        # time.sleep(1)
+
+
+
