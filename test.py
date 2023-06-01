ce6a030edfadc25c74e66c8826a1e8955ad0e1c2
azoghlam <123667148+azoghlam@users.noreply.github.com>
2023-06-01 12:11:45 -0400

    dawda

--- test.py
+++ test.py
@@ -8,9 +8,12 @@ AK8963_start()
 print('recording data')
 while 1:
     # print('mpu')
-    ax,ay,az,wx,wy,wz= mpu6050_conv() # read and convert mpu6050 data
-    # print("ak")
-    mx,my,mz,mt = AK8963_conv() # read and convert AK8963 magnetometer data
+    # ax,ay,az,wx,wy,wz= mpu6050_conv() # read and convert mpu6050 data
+    # # print("ak")
+    # mx,my,mz,mt = AK8963_conv() # read and convert AK8963 magnetometer data
+    ax = 1
+    ay = 2
+    az = 3
     # try:
     test = MPU9250(ax,ay,az)
 
@@ -19,11 +22,11 @@ while 1:
     # except:
     #     continue
     
-    print('{}'.format('-'*30))
-    print('accel [g]: x = {0:2.2f}, y = {1:2.2f}, z = {2:2.2f} '.format(ax ,ay ,az))
-    # print('accel [g]: x = {0:2.2f}, y = {1:2.2f}, z {2:2.2f}= '.format(ax,ay,az))
-    print('gyro [dps]:  x = {0:2.2f}, y = {1:2.2f}, z = {2:2.2f}'.format(wx,wy,wz))
-    print('mag [uT]:   x = {0:2.2f}, y = {1:2.2f}, z = {2:2.2f}'.format(mx,my,mz))
-    print('heading [uT]:   x = {0:2.2f}'.format(mt))
-    print('{}'.format('-'*30))
+    # print('{}'.format('-'*30))
+    # print('accel [g]: x = {0:2.2f}, y = {1:2.2f}, z = {2:2.2f} '.format(ax ,ay ,az))
+    # # print('accel [g]: x = {0:2.2f}, y = {1:2.2f}, z {2:2.2f}= '.format(ax,ay,az))
+    # print('gyro [dps]:  x = {0:2.2f}, y = {1:2.2f}, z = {2:2.2f}'.format(wx,wy,wz))
+    # print('mag [uT]:   x = {0:2.2f}, y = {1:2.2f}, z = {2:2.2f}'.format(mx,my,mz))
+    # print('heading [uT]:   x = {0:2.2f}'.format(mt))
+    # print('{}'.format('-'*30))
     time.sleep(1)
