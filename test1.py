77405ca9362940b85a5d1140b0fb00e2a7d52c92
azoghlam <123667148+azoghlam@users.noreply.github.com>
2023-06-01 17:08:16 -0400

    Update test1.py

--- test1.py
+++ test1.py
@@ -26,7 +26,7 @@ def initLoop ():
         except:
             continue
         
-        sio.emit('gyro',ax)
+        sio.emit('gyro',''+ax+','+ay+','+az)
         # sio.emit('acc',wx,wy,wz)
         # sio.emit('mag',mx,my,mz)
         # sio.emit('heading',heading)
